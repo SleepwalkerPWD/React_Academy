be5f359dffe2088fe0c4e9b1effe5d740ee56235
SleepwalkerPWD <37107643+SleepwalkerPWD@users.noreply.github.com>
2019-01-29 00:24:45 +0300

    first commit

---
+++ Homework_7/src/components/itemList/itemList.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import './itemList.css';
+export default class ItemList extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {characters} = this.props;
+        // console.log(this.characters);
+
+        return (
+            
+
+            <ul className="item-list list-group">
+                <li className="list-group-item">
+                    {characters}
+                </li>
+                <li className="list-group-item">
+                    John Snow
+                </li>
+                <li className="list-group-item">
+                    Brandon Stark
+                </li>
+                <li className="list-group-item">
+                    Geremy
+                </li>
+            </ul>
+        );
+    }
+}
